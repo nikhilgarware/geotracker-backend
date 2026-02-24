89eac0767332e6230ffc115f41b2feae98c697d3
Nikhil <nikhilgarware1198@gmail.com>
2026-02-24 14:01:46 +0530

    cors changes

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", // for local dev
-      "https://geo-tracker-frontend-self.vercel.app/",
+      "https://geo-tracker-frontend-self.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
